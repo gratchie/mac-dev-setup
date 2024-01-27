aba8fd4725fff6f79c4b90e4dcf279aa52735dc6
Grace Thompson <graziee@gmail.com>
2024-01-26 18:25:58 -0800

    cleanup

--- macos/init-playbook.sh
+++ macos/init-playbook.sh
@@ -1,8 +1,11 @@
 ## Run the playbook. Use the --check flag to run some tests before execution
 
+# Check
+# ANSIBLE_NOCOWS=1 ansible-playbook -i hosts.template main.yml --check --verbose --ask-become-pass
 
-# ANSIBLE_NOCOWS=1 ansible-playbook -i hosts.template main.yml --check --verbose
-
+# Execute
 ANSIBLE_NOCOWS=1 ansible-playbook -i hosts.template main.yml --verbose --ask-become-pass
 
+
+# Execute without sudo 
 # ANSIBLE_NOCOWS=1 ansible-playbook -i hosts.template main.yml --verbose
