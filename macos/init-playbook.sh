45b85e7533b0545c44e035f8787328e8e4d7d304
Grace Thompson <graziee@gmail.com>
2024-01-26 17:20:44 -0800

    include zsh plugins

--- macos/init-playbook.sh
+++ macos/init-playbook.sh
@@ -1,8 +1,8 @@
 ## Run the playbook. Use the --check flag to run some tests before execution
 
 
-ANSIBLE_NOCOWS=1 ansible-playbook -i hosts.template main.yml --check --verbose --ask-become-pass
+# ANSIBLE_NOCOWS=1 ansible-playbook -i hosts.template main.yml --check --verbose --ask-become-pass
 
-# ANSIBLE_NOCOWS=1 ansible-playbook -i hosts.template main.yml --verbose --ask-become-pass
+ANSIBLE_NOCOWS=1 ansible-playbook -i hosts.template main.yml --verbose --ask-become-pass
 
 # ANSIBLE_NOCOWS=1 ansible-playbook -i hosts.template main.yml --verbose
