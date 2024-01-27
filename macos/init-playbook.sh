e1e0f08c978b7032703b5d227d346a0ef7756108
Grace Thompson <g@gmail.com>
2024-01-26 16:08:46 -0800

    include p10k role

--- macos/init-playbook.sh
+++ macos/init-playbook.sh
@@ -1,4 +1,4 @@
 ## Run the playbook. Use the --check flag to run some tests before execution
-ANSIBLE_NOCOWS=1 ansible-playbook -i hosts.template main.yml --check --verbose
-# ANSIBLE_NOCOWS=1 ansible-playbook -i "${CLONE_DIR}"/hosts.template "${CLONE_DIR}"/main.yml --verbose
-~
+# ANSIBLE_NOCOWS=1 ansible-playbook -i hosts.template main.yml --check --verbose --ask-become-pass
+ANSIBLE_NOCOWS=1 ansible-playbook -i hosts.template main.yml --verbose --ask-become-pass
+# ANSIBLE_NOCOWS=1 ansible-playbook -i hosts.template main.yml --verbose
